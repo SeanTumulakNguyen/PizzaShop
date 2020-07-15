4c4a22a6960427b25a5bbf967cd7321f290361c0
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-07-14 23:25:17 -0400

    refactor globalstyle

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
 import Navbar from '../src/components/navbar/navbar';
 import Banner from '../src/components/banner/banner';
 import Menu from '../src/components/menu/menu';
-
-const GlobalStyle = createGlobalStyle`
- body {
-   margin: 0;
-  font-family: 'Open Sans', sans-serif;
- }
-
- h1, h2, h3 {
-  font-family: 'Righteous', cursive;
- }
- `;
+import { GlobalStyle } from './styles/GlobalStyle'
 
 function App() {
 	return (
@@ -22,7 +11,6 @@ function App() {
 			<Navbar />
 			<Banner />
 			<Menu />
-			<div>Hello Sliceline</div>
 		</React.Fragment>
 	);
 }
