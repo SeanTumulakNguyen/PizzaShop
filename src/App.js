088b5f228037c552d4e6a26c0c77dd76d2cabe85
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-07-17 00:35:59 -0400

    favicon and title

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import FoodDialog from '../src/components/foodDialog/foodDialog';
 import Order from '../src/components/order/order';
 import { useOpenFood } from '../src/components/hooks/useOpenFoods';
 import { useOrders } from '../src/components/hooks/useOrders';
+import { useTitle } from '../src/components/hooks/useTitle';
 
 function App() {
 	const openFood = useOpenFood();
 	const orders = useOrders();
+	useTitle({ ...openFood, ...orders });
 
 	return (
 		<React.Fragment>
